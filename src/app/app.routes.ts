c85e07a65853dd3fc6f3002aad88a4d0dc89de93
Roselyn Ocoy <rocoy56266@liceo.edu.ph>
2026-01-07 12:57:51 +0800

    Prelim Exam submission

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { Acitivity1Component } from './activity/activity-1.component';
+import { PrelimExamComponent } from './prelim-exam/prelim-exam.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    {
+        path: 'activity-1',
+        component: Acitivity1Component},
+
+        {
+          path: 'prelim-exam',
+          component: PrelimExamComponent
+        }
+
+
+ 
+];
+
